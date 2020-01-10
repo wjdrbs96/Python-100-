5c697390afa61e6f578ba91ab5bd728ca21f93cd
최정균 <wjdrbs966@naver.com>
2020-01-11 03:36:14 +0900

    62번 문제 풀기

---
+++ 50 ~ 100/62번.py
@@ -0,0 +1,30 @@
+a = ord('d')
+p = a + ord('Z')  # p = 190
+
+k = ord('n')-ord('d') # k =10
+c = ord('M')
+b = ord('d')
+d = ord('L') // ord('&')  # d = 2
+strone=""
+totalone=a-a
+sum=a-a
+total=a-a
+for i in range(ord('&')//d*(a//k)):
+    sum+=a
+
+
+for i in range(10):
+    total+=b
+
+for z in range(k):
+    totalone+=a
+
+v=sum+p+totalone
+
+totaltwo = totalone - ord('M')
+
+print(str(v)+str(totaltwo))
+
+
+
+
